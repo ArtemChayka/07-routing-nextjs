55fc69a0e0ea6e69a63c32146c62bd7a0422d66d
Artem Chaychenko <drchaychenko@gmail.com>
2025-09-01 14:06:39 +0300

    6

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,20 +1,19 @@
 import css from './layout.module.css';
+import React from 'react';
 
-interface FilterLayoutProps {
-  children: React.ReactNode;
-  sidebar: React.ReactNode;
-  modal: React.ReactNode;
-}
-
-export default function FilterLayout({
+export default function NotesFilterLayout({
   children,
   sidebar,
   modal,
-}: FilterLayoutProps) {
+}: {
+  children: React.ReactNode;
+  sidebar: React.ReactNode;
+  modal: React.ReactNode;
+}) {
   return (
     <div className={css.container}>
-      <aside className={css.sidebar}>{sidebar}</aside>
-      <main className={css.main}>{children}</main>
+      <div className={css.sidebar}>{sidebar}</div>
+      <div className={css.content}>{children}</div>
       {modal}
     </div>
   );
