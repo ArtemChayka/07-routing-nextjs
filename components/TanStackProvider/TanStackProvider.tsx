1f2017a47f150be3fac3490f87c3940b58327fc4
Artem Chaychenko <drchaychenko@gmail.com>
2025-09-01 15:43:34 +0300

    20

--- components/TanStackProvider/TanStackProvider.tsx
+++ components/TanStackProvider/TanStackProvider.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 const queryClient = new QueryClient();
 
-export default function TanStackProvider({ children }) {
+export default function TanStackProvider({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
